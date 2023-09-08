3ccf924168cb3c245a6a6349d86a0b2359371e47
tsfoverlord <rat4521@gmail.com>
2023-09-08 23:02:48 +0530

    Add convertToYodish() function which translates to yodish

--- advice.js
+++ advice.js
@@ -7,7 +7,20 @@ async function setAdvice(e){
     let response = await fetch('https://api.adviceslip.com/advice');
     let responseJson = await response.json();
     const advice = responseJson['slip']['advice'];
-    adviceElement.textContent = advice;
+    const yodishAdvice = await convertToYodish(advice);
+    adviceElement.textContent = yodishAdvice;
     this.textContent = 'Another';
     this.disabled = false;
+}
+
+async function convertToYodish(adviceText){
+    try {
+        const response = await fetch("https://api.funtranslations.com/translate/yoda.json?text="+adviceText);
+        result = await response.json();
+        return result['contents']['translated'];
+      } 
+
+      catch (error) {
+        console.error("Error:", error);
+      }
 }
